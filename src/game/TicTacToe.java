43c50af038ec7f270dad9332a6278187d69cef03
AnnaGubkina <gubkinatour@yandex>
2022-03-15 21:38:56 +0300

    xml parser added

--- src/game/TicTacToe.java
+++ src/game/TicTacToe.java
@@ -1,35 +1,53 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+package game;
+
+import gamefile.XMLReader;
+import gamefile.XMLWriter;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+import java.util.*;
 
 public class TicTacToe {
 
     public final static int SIZE = 3;
+    public final static int SIZE_FIELD = 9;
     public final static char EMPTY = '-';
     public final static char CROSS = 'X';
     public final static char ZERO = '0';
     public static Scanner scanner = new Scanner(System.in);
     public static Logger log = new Logger();
+    public static List<String> steps = new ArrayList<>();
+    public static String[] playerWinner;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException, IOException, SAXException {
 
-        HashMap<String, Integer> players = new HashMap<>();
-        System.out.println("Начинаем игру!");
-        System.out.println("Введите ваше имя для хода крестиком: ");
-        String player1 = scanner.nextLine();
-        System.out.println("Введите ваше имя для хода ноликом: ");
-        String player2 = scanner.nextLine();
-        players.put(player1, 0);
-        players.put(player2, 0);
+//        HashMap<String, Integer> players = new HashMap<>();
+//        System.out.println("Начинаем игру!");
+//        System.out.println("Введите ваше имя для хода крестиком: ");
+//        String player1 = scanner.nextLine();
+//
+//        System.out.println("Введите ваше имя для хода ноликом: ");
+//        String player2 = scanner.nextLine();
+//
+//        fillOfList();
+//
+//        players.put(player1, 0);
+//        players.put(player2, 0);
+//
+//        playStart(player1, player2, players);
+//        playAgain(player1, player2, players);
+//
+//        XMLWriter.write(player1, player2, steps, playerWinner);
 
-        playStart(player1, player2, players);
-        playAgain(player1, player2, players);
+        XMLReader.readXML();
 
 
     }
 
     public static void playStart(String player1, String player2, HashMap<String, Integer> players) {
-
+        int count = 0;
         char[][] field = new char[SIZE][SIZE];
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -42,7 +60,8 @@ public class TicTacToe {
         while (true) {
             System.out.println("Ход " + (isCrossTurn ? "крестиком " : "ноликом ") + ",введите 2 цифры поля(например 12)");
             printField(field);
-            String input = scanner.nextLine(); //3,2
+            String input = scanner.nextLine(); //32
+            steps.set(count, input);
             String[] parts = input.split("");
             int x = Integer.parseInt(parts[0]) - 1; // 3-1=2
             int y = Integer.parseInt(parts[1]) - 1; // 2-1=1
@@ -53,12 +72,15 @@ public class TicTacToe {
             }
             //совершаем ход
             field[x][y] = isCrossTurn ? CROSS : ZERO;
+            count++;
             if (isWin(field, isCrossTurn ? CROSS : ZERO)) {
                 System.out.println("Выиграл " + (isCrossTurn ? player1 : player2) + "!");
                 if (isCrossTurn) {
                     players.put(player1, players.get(player1) + 1);
+                    playerWinner = new String[]{"1", player1, "X"};
                 } else {
                     players.put(player2, players.get(player2) + 1);
+                    playerWinner = new String[]{"2", player2, "0"};
                 }
                 printField(field);
                 //завершаем игру
@@ -122,6 +144,13 @@ public class TicTacToe {
                 System.out.print(cell + " ");
             }
             System.out.println();
+        }
+    }
+
+    public static void fillOfList() {
+        //заполняем лист шагов (для создания XML) первоначальными значениями
+        for (int i = 0; i < SIZE_FIELD; i++) {
+            steps.add("-");
         }
     }
 }
---
+++ src/gamefile/Step.java
@@ -0,0 +1,18 @@
+package gamefile;
+
+public class Step {
+
+    String numStep;
+    String value;
+
+    public Step(String numStep, String value) {
+        this.numStep = numStep;
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+
+}
